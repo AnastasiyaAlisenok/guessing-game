46b30abab3c4bcb393bc124ce1cb51f4644f7c76
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-02-17 01:40:39 +0300

    feat: add js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
+
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor(minNum, maxNum) {
+        this.minNum;
+        this.maxNum;
     }
+    
+    setRange(min, max) {
+        this.minNum = min;
+        this.maxNum = max;
+    };
 
     guess() {
-
-    }
+        return Math.round((this.minNum + (this.maxNum -this.minNum)/2));
+        
+    };
 
     lower() {
-
+        this.maxNum = this.guess();
     }
 
     greater() {
-
+        this.minNum = this.guess();
     }
 }
 
